596421722f1c25d1785393249c6f2c5070a471c1
Ketan Patil <mail.ketan027@gmail.com>
2025-02-20 17:56:15 +0530

    restarting frontend again

---
+++ backend/middleware/verifyToken.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken"
+
+export const verifyToken= (req,res,next)=>{
+    const token= req.cookies.token
+    if(!token ) return res.status(400).json({success:false,message:"Unauthorized-no token Provided"})
+        try {
+    const decoded=jwt.verify(token,process.env.JWT_SECRET)
+    req.userId=decoded.userId;
+    next()
+            
+        } catch (error) {
+            console.error("ERROR",error)
+        }
+}
