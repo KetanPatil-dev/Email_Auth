2bd7f5224b5a0c863dfbe0e9829e0c8b0f0cba26
ketan <mail.ketan027@gmail.com>
2025-06-04 14:01:14 +0530

    Implemented nodemailer with templates for sending emails

---
+++ server/mailtrap/mail.js
@@ -0,0 +1,30 @@
+import { Verification_Email_Template,Welcome_Email_Template } from "./EmailTemplate.js"
+import { transporter } from "./mailtrap.config.js"
+
+export const SendVerificationCode=async(email,verificationCode,name)=>{
+    try {
+          const info = await transporter.sendMail({
+            from: '"Ketan Patil" <mail.ketan027@gmail.com>',
+            to: "mail.ketan027@gmail.com",
+            subject: "Verify",
+            text: "Hello world?", // plain‑text body
+            html: Verification_Email_Template.replace("{verificationCode}",verificationCode).replace("{name}",name)
+          })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const WelcomeEmail=async(email,name)=>{
+    try {
+          const info = await transporter.sendMail({
+            from: '"Ketan Patil" <mail.ketan027@gmail.com>',
+            to: "mail.ketan027@gmail.com",
+            subject: "Welcome",
+            text: "Welcome Email", // plain‑text body
+            html: Welcome_Email_Template.replace("{name}",name)
+          })
+    } catch (error) {
+        console.log(error)
+    }
+}
--- server/mailtrap/mailtrap.config.js
+++ server/mailtrap/mailtrap.config.js
@@ -1,6 +1,6 @@
 import nodemailer from "nodemailer"
 
-const transporter = nodemailer.createTransport({
+export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false, // true for 465, false for other ports
@@ -10,19 +10,3 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-const SendEmail=async()=>{
-    try {
-        const info = await transporter.sendMail({
-    from: '"Ketan Patil" <mail.ketan027@gmail.com>',
-    to: "mail.ketan027@gmail.com",
-    subject: "Hello ✔",
-    text: "Hello world?", // plain‑text body
-    html: "<b>Hello world?</b>",
-        } )  
-        console.log(info)
-    }   
-     catch (error) {
-        console.log(error)
-    }
-}
-SendEmail()
